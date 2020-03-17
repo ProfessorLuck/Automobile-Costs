1a0f962e34e162b11f5e6b464c46fa1a99cbbec1
professorluck <joshuacollins@Joshuas-MacBook-Pro.local>
2020-03-17 12:54:51 -0400

    initial commit

---
+++ Automobile Costs/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace Automobile_Costs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.loanTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.insTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.gasTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.oilTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tiresTextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.maintenanceTextBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.annualOutput = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Insert Monthly Costs:";
+            // 
+            // loanTextBox
+            // 
+            this.loanTextBox.Location = new System.Drawing.Point(172, 36);
+            this.loanTextBox.Name = "loanTextBox";
+            this.loanTextBox.Size = new System.Drawing.Size(100, 20);
+            this.loanTextBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(88, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Loan Payment:";
+            // 
+            // insTextBox
+            // 
+            this.insTextBox.Location = new System.Drawing.Point(172, 73);
+            this.insTextBox.Name = "insTextBox";
+            this.insTextBox.Size = new System.Drawing.Size(100, 20);
+            this.insTextBox.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(109, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Insurance:";
+            // 
+            // gasTextBox
+            // 
+            this.gasTextBox.Location = new System.Drawing.Point(172, 109);
+            this.gasTextBox.Name = "gasTextBox";
+            this.gasTextBox.Size = new System.Drawing.Size(100, 20);
+            this.gasTextBox.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(137, 109);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Gas:";
+            // 
+            // oilTextBox
+            // 
+            this.oilTextBox.Location = new System.Drawing.Point(172, 144);
+            this.oilTextBox.Name = "oilTextBox";
+            this.oilTextBox.Size = new System.Drawing.Size(100, 20);
+            this.oilTextBox.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(144, 144);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(22, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Oil:";
+            // 
+            // tiresTextBox
+            // 
+            this.tiresTextBox.Location = new System.Drawing.Point(172, 180);
+            this.tiresTextBox.Name = "tiresTextBox";
+            this.tiresTextBox.Size = new System.Drawing.Size(100, 20);
+            this.tiresTextBox.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(133, 180);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(33, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Tires:";
+            // 
+            // maintenanceTextBox
+            // 
+            this.maintenanceTextBox.Location = new System.Drawing.Point(172, 215);
+            this.maintenanceTextBox.Name = "maintenanceTextBox";
+            this.maintenanceTextBox.Size = new System.Drawing.Size(100, 20);
+            this.maintenanceTextBox.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(94, 215);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(72, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Maintenance:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(64, 257);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(107, 20);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Monthly Total:";
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.outputLabel.Location = new System.Drawing.Point(173, 257);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(100, 23);
+            this.outputLabel.TabIndex = 14;
+            this.outputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(198, 322);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // annualOutput
+            // 
+            this.annualOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.annualOutput.Location = new System.Drawing.Point(172, 296);
+            this.annualOutput.Name = "annualOutput";
+            this.annualOutput.Size = new System.Drawing.Size(100, 23);
+            this.annualOutput.TabIndex = 16;
+            this.annualOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(69, 296);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(102, 20);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Annual Total:";
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(117, 322);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 18;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 357);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.annualOutput);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.maintenanceTextBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.tiresTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.oilTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.gasTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.insTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.loanTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Automobile Costs";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox loanTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox insTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox gasTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox oilTextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tiresTextBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox maintenanceTextBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label annualOutput;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button clearButton;
+    }
+}
+
---
+++ Automobile Costs/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Automobile_Costs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double loan = double.Parse(loanTextBox.Text); //gets input
+            double insurance = double.Parse(insTextBox.Text);
+            double gas = double.Parse(gasTextBox.Text);
+            double oil = double.Parse(oilTextBox.Text);
+            double tires = double.Parse(tiresTextBox.Text);
+            double maintenance = double.Parse(maintenanceTextBox.Text);
+
+            double monthly = loan + insurance + gas + oil + tires + maintenance; //calculates totals
+            double annual = monthly * 12;
+
+            outputLabel.Text = monthly.ToString("c"); //displays output
+            annualOutput.Text = annual.ToString("c");
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            loanTextBox.Text = ""; //clears textboxes
+            insTextBox.Text = "";
+            gasTextBox.Text = "";
+            oilTextBox.Text = "";
+            tiresTextBox.Text = "";
+            maintenanceTextBox.Text = "";
+
+            outputLabel.Text = "";   //clears output labels
+            annualOutput.Text = "";
+        }
+    }
+}
